03c489fb8abf3908b9927ed1beb366ea0357ac89
Roman Bek <r.bek@student.uq.edu.au>
2025-10-13 15:27:50 +1000

    Add DoubleConv block + runnable UNet2D stub (to be replaced with full UNet)

--- recognition/unet3d_48092360/modules.py
+++ recognition/unet3d_48092360/modules.py
@@ -1,2 +1,31 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+class DoubleConv(nn.Module):
+    """Two stacked 3x3 convs with BN+ReLU"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1 ,1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+    
+class UNet2D(nn.Module):
+    """Temporary stub so train/predict run"""
+    def __init__(self, in_channels: int = 1, num_classes: int = 1):
+        super().__init__()
+        self.head = nn.Conv2d(in_channels, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.head(x)
+
 class UNet2D: pass
 class UNet3D: pass
